6f9116f053d762b585d14edbbea1d1fa7b3aab8f
Val <regval@pm.me>
2023-01-01 17:48:03 +0800

    add comments to rpc client, save closing

--- fx/cmd/client_get_fx/main.go
+++ fx/cmd/client_get_fx/main.go
@@ -1,24 +1,44 @@
-package client_get_fx
+package main
 
 import (
 	"context"
+	"flag"
 	r "github.com/sh-valery/microservices-logging/internal/rpc_gen"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 )
 
-func main() {
-	// todo: 1. complete client connection, to the server
-	client := r.NewFxServiceClient(grpc.ClientConnInterface())
+var host = flag.String("host", "localhost:50051", "server address for connection, default: localhost:50051")
 
-	})
+func main() {
+	flag.Parse()
+
+	// init rpc connection
+	conn, err := grpc.Dial(*host, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("connection error: %v", err)
+	}
+	defer func() {
+		err = conn.Close()
+		if err != nil {
+			log.Fatalf("connection close error: %v", err)
+		}
+	}()
+
+	client := r.NewFxServiceClient(conn)
+
+	// build request
 	request := &r.FxServiceRequest{
 		SourceCurrencyCode: "USD",
 		TargetCurrencyCode: "CHF",
 	}
+	log.Printf("request from the client: %+v", request)
+
+	// send request
 	response, err := client.GetFxRate(context.Background(), request)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("response from the rpc server: %v", response)
+	log.Printf("response from the rpc server: %+v", response)
 }
